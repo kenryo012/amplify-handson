fc52e0f46c15f1c9757449ad611b1146bb6ec45a
kenryo012 <zuotengxianliang012@gmail.com>
2023-11-05 03:01:38 +0900

    First Commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { NavBarHeader2, MarketingPricing, MarketingFooter } from './ui-components'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarHeader2 width={"100vw"}/>
+      <MarketingPricing width={"100vw"}/>
+      <MarketingFooter width={"100vw"}/>
     </div>
   );
 }
